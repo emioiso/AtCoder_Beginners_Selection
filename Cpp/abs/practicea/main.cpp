b1870f75daaa3fbb2fd955a46a796530094958b8
emioiso <jijjjiijiijijiji@gmail.com>
2023-01-22 19:41:57 +0900

    PracticeA - Welcome to AtCoder

---
+++ Cpp/abs/practicea/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int	main()
+{
+	int	a,b,c;
+
+	cin >> a >> b >> c;
+	// cout << a << " " << b << " " << c <<endl;
+
+	cout << a + b + c << " " ;
+
+	string s;
+	cin >> s;
+	cout << s <<endl;
+
+}
