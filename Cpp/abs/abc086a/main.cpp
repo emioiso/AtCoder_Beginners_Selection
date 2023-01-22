ac32969f008b806bf6a02550f9e8e331432ed708
emioiso <jijjjiijiijijiji@gmail.com>
2023-01-22 19:55:41 +0900

    ABC086A - Product

---
+++ Cpp/abs/abc086a/main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int	main()
+{
+	int	a,b;
+
+	cin >> a >> b;
+	// cout << a << " " << b <<endl;
+
+	if(a * b % 2 == 0){
+		cout << "Even" <<endl;
+	} else {
+		cout << "Odd" <<endl;
+	}
+
+}
